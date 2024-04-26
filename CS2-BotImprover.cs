6306a335f2aa9c207f19bf561f1ac8608ab285f5
himenekocn <xubw@vip.qq.com>
2024-04-26 17:23:28 +0800

    test

---
+++ CS2-BotImprover.cs
@@ -0,0 +1,39 @@
+using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
+using CounterStrikeSharp.API.Modules.Commands;
+using CounterStrikeSharp.API.Modules.Utils;
+using CounterStrikeSharp.API.Modules.Memory;
+using CounterStrikeSharp.API.Modules.Memory.DynamicFunctions;
+
+using Microsoft.Extensions.Logging;
+
+namespace BotImprover;
+public class BotImprover : BasePlugin
+{
+    public override string ModuleName => "CS2 BotImprover Plugin";
+
+    public override string ModuleVersion => "0.0.1";
+
+    public override string ModuleAuthor => "LynchMus";
+
+    public override string ModuleDescription => "BotImprover plugin";
+
+    private MemoryFunctionWithReturn<float> CCSBot_UpKeepFunc =
+        new("55 48 89 E5 41 57 41 56 41 55 41 54 49 89 FC 53 48 83 EC 38 4C 8B 2D 7D D9 FA 00", Addresses.ServerPath);
+
+    public override void Load(bool hotReload)
+    {
+        Console.WriteLine("======================================");
+        Console.WriteLine("HIME BotImprover Load Start!");
+        CCSBot_UpKeepFunc.Hook(Hook_CCSBot_UpKeep, HookMode.Pre);
+        Console.WriteLine("HIME BotImprover Load Finish!");
+        Console.WriteLine("======================================");
+    }
+
+    private HookResult Hook_CCSBot_UpKeep(DynamicHook hook)
+    {
+        hook.SetReturn(0);
+        return HookResult.Changed;
+    }
+}
